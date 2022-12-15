76340d7e6f1d15164f5ffb30e1d9c1c8186a4d9e
vitoria lopes <heyvitoria.lopes@gmail.com>
2022-12-14 23:47:10 -0300

    new: employee interface

---
+++ src/types/employee.ts
@@ -0,0 +1,13 @@
+export interface Employee {
+  cpf: string;
+  phoneNumber: string;
+  email: string;
+  role: string;
+  vr: number;
+  vt: number;
+  healthPlan: string;
+  salary: number;
+  address: string;
+  firstName: string;
+  lastName: string;
+}
